4e191938c4d1d75f49d7646663cd98d4bba8afe5
iaraolivera <dev.iaraolivera@gmail.com>
2022-12-16 16:23:32 -0300

    primer commit

---
+++ src/Components/WeatherMainInfo.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./WeatherMainInfo.module.css";
+
+function WeatherMainInfo({ weather }) {
+  return (
+    <div className={styles.mainInfo}>
+      <div className={styles.city}>{weather?.location.name}</div>
+      <div className={styles.country}>{weather?.location.country}</div>
+      <div className={styles.row}>
+        <div>
+          <img
+            src={`http:${weather?.current.condition.icon}`}
+            width="128"
+            alt={weather?.current.condition.text}
+          />
+        </div>
+        <div className={styles.weatherCondition}>
+          <div className={styles.condition}>
+            {weather?.current.condition.text}
+          </div>
+          <div className={styles.current}>{weather?.current.temp_c} °</div>
+        </div>
+      </div>
+      {/* mapa */}
+      <iframe
+        title="mapa"
+        src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d317716.6064697738!2d${weather?.location.lon}1!3d${weather?.location.lat}5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1670214613147!5m2!1ses!2sar`}
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  );
+}
+
+export default WeatherMainInfo;
